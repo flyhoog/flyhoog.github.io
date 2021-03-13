84f945e60ce9f2ccff3b6a9958d0b6791a959b1e
navaar <78893363+flyhoog@users.noreply.github.com>
2021-03-13 20:56:04 +0530

    abc

---
+++ js/custom.js
@@ -0,0 +1,27 @@
+(function ($) {
+
+    "use strict";
+
+        // PRE LOADER
+        $(window).load(function(){
+          $('.preloader').delay(500).slideUp('slow'); // set duration in brackets    
+        });
+
+
+        // MENU
+        $('.navbar-collapse a').on('click',function(){
+          $(".navbar-collapse").collapse('hide');
+        });
+
+        $(window).scroll(function() {
+          if ($(".navbar").offset().top > 50) {
+            $(".navbar-fixed-top").addClass("top-nav-collapse");
+              } else {
+                $(".navbar-fixed-top").removeClass("top-nav-collapse");
+              }
+        });
+
+
+      
+
+})(jQuery);
